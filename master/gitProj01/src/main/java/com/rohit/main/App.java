1eecc3383916d7e9a8c07451c125d95da51d121c
Rohit992291 <rohitshelke10012003@gmail.com>
2025-05-01 09:43:04 +0530

    initail stage

---
+++ master/gitProj01/src/main/java/com/rohit/main/App.java
@@ -0,0 +1,13 @@
+package com.rohit.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
